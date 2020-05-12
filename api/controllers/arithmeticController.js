9597fa5742e39a5e24ba93309dc6ee8418dcc321
ktownpete <65220693+ktownpete@users.noreply.github.com>
2020-05-12 02:03:40 -0700

    Added comment to explain the addition
    code is working but is confusing

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -11,7 +11,8 @@ exports.calculate = function(req, res) {
   });
 
   var operations = {
-    'add':      function(a,b) { return a + b },
+    //this is a comment... do not remove the plus signs since it will totaly break...
+    'add':      function(a,b) { return +a + +b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
